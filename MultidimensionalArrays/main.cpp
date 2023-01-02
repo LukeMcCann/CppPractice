b9ce878e35886cd89c4874b6f1ea440559fc82b2
LukeMcCann <developer-lukemccann@outlook.com>
2023-01-02 18:56:57 +0000

    Add array iteration example

--- MultidimensionalArrays/main.cpp
+++ MultidimensionalArrays/main.cpp
@@ -16,5 +16,17 @@ int main()
         cout << endl;
     }
 
+    // We can find the length of an array using sizeof
+    // sizeof will return the number of bytes in memory
+    // used by our array, we can divide this by the
+    // number of bytes used by a single element to find
+    // the number of items in our array.
+
+    int values[] = {1, 2, 3};
+
+    for (int k = 0; k < sizeof(values)/sizeof(int); k++) {
+        cout << "Element " << k << ": " << values[k] << endl;
+    }
+
     return 0;
 }
