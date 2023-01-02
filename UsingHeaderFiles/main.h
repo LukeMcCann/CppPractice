3b533dfa38123bae2c26dbf922ad97301c998455
LukeMcCann <developer-lukemccann@outlook.com>
2023-01-02 20:43:17 +0000

    Further notes

--- UsingHeaderFiles/main.h
+++ UsingHeaderFiles/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H_ // Preprocessor directives start with a hash
 #define MAIN_H_ // Preprocessor will run as the first state
                 // looking for commands beginning with #
-                // it will then run these commands
+                // it will then run these commands.
+
+                // These specific commands define a symbol MAIN_H_
+                // if the symbol is not already defined.
 void doSomething();
 
 #endif // !MAIN_H_
