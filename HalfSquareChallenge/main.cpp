f928b2726754cf5c26d034997c0fb237d85240fc
LukeMcCann <developer-lukemccann@outlook.com>
2023-01-02 19:37:26 +0000

    Improve solution with algebraic expression

--- HalfSquareChallenge/main.cpp
+++ HalfSquareChallenge/main.cpp
@@ -4,14 +4,12 @@ using namespace std;
 
 int main()
 {
-    int squareColumns = 5;
     int squareRows = 5;
 
     for (unsigned int i = 0; i < squareRows; i++) {
-        for (unsigned int j = 0; j < squareColumns; j++) {
+        for (unsigned int j = 0; j < 6 - i; j++) {
             cout << '#' << flush;
         }
         cout << endl;
-        squareColumns--;
     }
 }
