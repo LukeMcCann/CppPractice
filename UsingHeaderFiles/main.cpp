84db90ef528309448babfc2e40bdcc1869a13403
LukeMcCann <developer-lukemccann@outlook.com>
2023-01-02 20:40:37 +0000

    Expand notes

--- UsingHeaderFiles/main.cpp
+++ UsingHeaderFiles/main.cpp
@@ -3,6 +3,13 @@
 #include "main.h" // Tells the preprocessor that we want to use
                  // the functions in main.h
 
+                 // We use double quotes for these includes
+                 // angle brackets refer to files in a standard location
+                 // which the compiler will know about.
+
+                 // Double quotes refer to the files included in the
+                 // project.
+
 using namespace std;
 
 int main()
