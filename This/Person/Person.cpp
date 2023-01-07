ff17ce89574b577a4d2d6f4090d0cbab788d41e6
LukeMcCann <developer-lukemccann@outlook.com>
2023-01-07 22:20:35 +0000

    add note for this

--- This/Person/Person.cpp
+++ This/Person/Person.cpp
@@ -15,6 +15,9 @@ Person::Person(string name, int age)
 {
     this->name = name;
     this->age = age;
+
+    // This refers to the location in memory
+    cout << "Location in memory: " << this << endl;
 }
 
 string Person::toString()
